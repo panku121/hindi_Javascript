e6f351469ab88cce634c14274fa267451b16ef62
Pankaj Kumar <87565770+panku121@users.noreply.github.com>
2024-03-14 10:33:34 +0000

    Why not to use the var in javaScript

---
+++ basic_js/variable.js
@@ -0,0 +1,11 @@
+const accountId = 122254;
+let accountEmail = "pankaj@test.com";
+var accountPassword = "12Three"
+
+// accountId = 55;
+
+console.log(accountId);
+
+/*
+In javaScript is prefered not to use the var as the var declared variable is not able to identify the block scope. 
+*/
